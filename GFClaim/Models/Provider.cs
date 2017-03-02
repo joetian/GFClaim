5f9fee7e1134a28a1d3a079d81c1048b57facb70
joe <joetian@gmail.com>
2017-03-01 16:08:52 -0800

    Add notation on Provider

--- GFClaim/Models/Provider.cs
+++ GFClaim/Models/Provider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,23 @@ namespace GFClaim.Models
     public class Provider
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
         public string Name { get; set; }
+
         public string Address1 { get; set; }
+        
         public string Address2 { get; set; }
+        
         public string City { get; set; }
+        
         public string State { get; set; }
+        
         public string Zipcode { get; set; }
+        
         public string Phone { get; set; }
+        
         public string TaxId { get; set; }
     }
 }
