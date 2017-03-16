84206e2712689061174d60685b67d6dc9242160a
joe <test@mail.com>
2017-03-16 07:58:48 -0700

    add domain

---
+++ OnlineShoppingStore/OnlineShoppingStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShoppingStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EFDbContext : DbContext   // exactly same as web.config.connectionString
+    {
+        public DbSet<Product> Products { get; set; }     // map to all tables in DB
+
+    }
+}
